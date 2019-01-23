1a8309bfb2a94900171a7fd3dab11243ed2c6d58
andreybannikov <andrewnnov@yandex.ru>
2019-01-23 19:22:15 +0300

    dependency injection code

--- spring-demo-one/src/com/luv2code/springdemo/BaseBallCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/BaseBallCoach.java
@@ -2,8 +2,24 @@ package com.luv2code.springdemo;
 
 public class BaseBallCoach implements Coach {
 	
+	//define a private field for the dependency
+	private FortuneService fortuneService;
+	
+	
+	//define a constructor for dependency injection
+	
+	public BaseBallCoach(FortuneService theFortuneService) {		
+		fortuneService = theFortuneService;
+	}
+	
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on batting practice";
+	}	
+
+	@Override
+	public String getDailyFortune() {
+		//use my fortuneService to get a fortune
+		return fortuneService.getFortune();
 	}
 
 }
--- spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -1,11 +1,29 @@
 package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	
+
+	public TrackCoach(FortuneService fortuneService) {		
+		this.fortuneService = fortuneService;
+	}
+	
+	public TrackCoach() {
+		
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		
 		return "Run a hard 5K";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return "Just do it: " +  fortuneService.getFortune();
 	}
 
 }
