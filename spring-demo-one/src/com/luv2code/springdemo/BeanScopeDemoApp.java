82e95d4f27db78dee7ad003d5030edda4c0455d1
andreybannikov <andrewnnov@yandex.ru>
2019-01-24 11:52:25 +0300

    singlton and prototype scope

---
+++ spring-demo-one/src/com/luv2code/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,35 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		//load the spring config file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		//retrieve bean from container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+		
+		//check if thew are the same
+		boolean result = (theCoach==alphaCoach);
+		
+		//print out the result
+		
+		System.out.println("\nPointing to the same obect: " + result);
+		
+		System.out.println("\nMemory location for theCoach: " + theCoach);
+		
+		System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
+		
+		//close the context
+		context.close();
+		
+
+	}
+
+}
