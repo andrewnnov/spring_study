ad54edaf547a1d25cc76cb9f728da68861dff58f
andreybannikov <andrewnnov@yandex.ru>
2019-01-25 08:44:00 +0300

    @Autowired for setter injection

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -8,10 +8,22 @@ public class TennisCoach implements Coach {
 	
 	private FortuneService fortuneService;
 	
+	public TennisCoach() {
+		System.out.println(">>Tennis coach: inside default constructor");
+	}
+	
+	//define a setter method
 	@Autowired
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println(">> Tennis coach: inside default setFortuneService() method");
+		fortuneService = theFortuneService;
+	}	
+	
+	/*@Autowired
 	public TennisCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
+	*/
 
 	@Override
 	public String getDailyWorkout() {		
