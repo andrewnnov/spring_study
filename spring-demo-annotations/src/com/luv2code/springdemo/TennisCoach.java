709ef1828089a7a5f87e8f163839db7ece94640a
andreybannikov <andrewnnov@yandex.ru>
2019-01-25 10:08:47 +0300

    LifeCycleBean @PostConstruct and @PreDestroy

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,12 +1,14 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component            //default id it is "tennisCoach"
-@Scope("prototype")
 public class TennisCoach implements Coach {
 	
 	@Autowired
@@ -15,6 +17,18 @@ public class TennisCoach implements Coach {
 	
 	public TennisCoach() {
 		System.out.println(">>Tennis coach: inside default constructor");
+	}
+	
+	//define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
+	}
+	
+	//define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">>TennisCoach: inside of doMyCleanupStuff()");
 	}
 	
 	/*
