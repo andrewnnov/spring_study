9c1e66dcadb717fda2669e24f675da006f818f74
andreybannikov <andrewnnov@yandex.ru>
2019-01-25 13:50:06 +0300

    spring configuration with java code2

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -0,0 +1,27 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SwimJavaConfigDemoApp {
+
+	public static void main(String[] args) {
+		
+		//read spring config java class		
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(SportConfig.class);
+		
+		//get the bean from spring container		
+		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		
+		//call the method on the bean		
+		System.out.println(theCoach.getDailyWorkout());
+		
+		//call method to get the daily fortune		
+		System.out.println(theCoach.getDailyFortune());
+		
+		//close the context
+		context.close();
+	}
+
+}
