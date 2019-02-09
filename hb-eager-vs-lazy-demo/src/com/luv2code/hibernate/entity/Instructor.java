0b990ad983c33f4ba82fc0abcaf944b70ed90737
andreybannikov <andrewnnov@yandex.ru>
2019-02-09 22:06:07 +0300

    hb-eager-vs-lazy

---
+++ hb-eager-vs-lazy-demo/src/com/luv2code/hibernate/entity/Instructor.java
@@ -0,0 +1,145 @@
+package com.luv2code.hibernate.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="instructor")
+public class Instructor {
+	
+	    //annotate the class as an entity and map to db table	
+		
+		//define the fields
+		
+		//annotate the fields with db column names	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;	
+	
+	 //**set up mapping to InstructorDetail entity	
+	
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="instructor_detail_id")
+	private InstructorDetail instructorDetail;
+	
+	@OneToMany(fetch=FetchType.EAGER,
+			mappedBy="instructor",
+			cascade={CascadeType.PERSIST,
+					CascadeType.MERGE,
+			        CascadeType.DETACH,
+			        CascadeType.REFRESH})
+	private List<Course> course;
+	
+	    //create constructors
+	
+		//generate getter and setter method
+	
+			
+		//generate toString() method
+	
+	 public Instructor() {
+		 
+	 }
+
+		public Instructor(String firstName, String lastName, String email) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.email = email;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public InstructorDetail getInstructorDetail() {
+			return instructorDetail;
+		}
+
+		public void setInstructorDetail(InstructorDetail instructorDetail) {
+			this.instructorDetail = instructorDetail;
+		}
+
+		@Override
+		public String toString() {
+			return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+					+ ", instructorDetail=" + instructorDetail + "]";
+		}
+
+		public List<Course> getCourse() {
+			return course;
+		}
+
+		public void setCourse(List<Course> course) {
+			this.course = course;
+		}  
+		
+		
+		//add convenience method for bi-directional relationship
+		
+		public void add(Course tempCourse) {
+			if(course == null) {
+				course = new ArrayList<>();
+			}			
+			course.add(tempCourse);
+			tempCourse.setInstructor(this);
+		}
+		
+		
+		
+		
+		
+		
+		
+
+}
