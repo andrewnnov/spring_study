6fdda68cc3d4d4cd81ef31e91bf8dcc43b23a1fc
andreybannikov <andrewnnov@yandex.ru>
2019-05-02 18:54:24 +0300

    add new customer

--- mvc-customer-tracker/WebContent/WEB-INF/view/list-customers.jsp
+++ mvc-customer-tracker/WebContent/WEB-INF/view/list-customers.jsp
@@ -1,12 +1,63 @@
-<!DOCTYPE html>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+
+<!Doctype html>
+
 <html>
+
 <head>
-<meta charset="ISO-8859-1">
-<title>List Customers</title>
+      <title>List Customers</title>
+      <link type="text/css"
+       rel="stylesheet"
+        href="${pageContext.request.contextPath}/resources/css/style.css" />
 </head>
+
 <body>
 
-List Customers- coming soon
+      <div id="wrapper">
+           <div id="header">
+                <h2>CRM - Customer Relationship Manager</h2>
+           </div>
+      </div>
+     
+          <div id="container">
+     
+     <div id="content">
+     
+     <!-- put new button: Add Customer -->  
+     
+     <input type="button" value="Add Customer"
+            onclick="window.location.href='showFormForAdd'; return false;"
+            class="add-button"
+      />
+      
+     
+     <!-- add our html table here -->
+     <table>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>            
+            </tr>
+            
+            <!-- loop over and print our customers -->
+            <c:forEach var="tempCustomer" items="${customers}">
+            
+                 <tr>
+                    <td> ${tempCustomer.firstName} </td>
+                    <td> ${tempCustomer.lastName} </td>
+                    <td> ${tempCustomer.email} </td>
+                 
+                 </tr>    
+            
+            </c:forEach>
+     
+     
+     </table>
+     
+     </div>
+     
+     </div>
 
 </body>
 </html>
--- mvc-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ mvc-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,19 +1,59 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.sevice.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
-	@RequestMapping("/list")
+	//need to inject our customer service
+	@Autowired
+	private CustomerService customerService;
+	
+	
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
 		
+		//get customer from the service
+		List<Customer> theCustomer = customerService.getCustomers();
+		
+		//add the customer to the model
+		theModel.addAttribute("customers", theCustomer);
 		
 		
 		return "list-customers";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		//create model attribute to bind form data
+		Customer theCustomer = new Customer();
+		theModel.addAttribute("customer", theCustomer);
+		
+		return "customer-form";
+		 
+	}
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+		
+		
+		//save the customer using our service
+		customerService.saveCustomer(theCustomer);
+		
+		return "redirect:/customer/list";
 	}
 
 }
